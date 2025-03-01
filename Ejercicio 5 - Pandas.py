46557424d9440f25f56a2a8eab9985046cee4713
Elizabeth Borbon <elyborboon@gmail.com>
2025-03-01 10:44:01 -0600

    Add files via upload

---
+++ Ejercicio 5 - Pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+serie1 = pd.Series([1,2,3,5,7])
+serie2 = pd.Series([13,17,11,5,3])
+print (serie1)
+print (serie2)
+print (serie1 < serie2)
+print (serie1 > serie2)
+print (serie1 == serie2)
